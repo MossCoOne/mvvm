3a9577a6672ac88ad1a176b1aee50c071589c693
Mossco MacCintosh <mosscomoabelo@gmail.com>
2020-05-21 20:56:15 +0200

    Add List and Detailed fragments respectively

---
+++ app/src/main/java/com/mossco/za/mvvm/view/LandingActivity.kt
@@ -0,0 +1,13 @@
+package com.mossco.za.mvvm.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mossco.za.mvvm.R
+
+class LandingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing)
+    }
+}
