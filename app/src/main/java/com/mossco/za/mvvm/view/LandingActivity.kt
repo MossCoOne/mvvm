a9f31f25ea3585d85f89b0477a6890820a94be59
Mossco MacCintosh <mosscomoabelo@gmail.com>
2020-05-24 20:51:13 +0200

    Configure Navigation controller to show back button

--- app/src/main/java/com/mossco/za/mvvm/view/LandingActivity.kt
+++ app/src/main/java/com/mossco/za/mvvm/view/LandingActivity.kt
@@ -2,12 +2,27 @@ package com.mossco.za.mvvm.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.mossco.za.mvvm.R
+import com.mossco.za.mvvm.databinding.ActivityLandingBinding
 
 class LandingActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityLandingBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_landing)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_landing)
+
+        navController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
